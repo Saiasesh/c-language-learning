1742cfba8865c94243289358c8f36b577e94dc6a
Saiasesh <medhosaiasesh@gmail.com>
2025-09-26 03:09:45 -0700

    Create Tables.c

---
+++ Tables.c
@@ -0,0 +1,12 @@
+// table of a number
+#include <stdio.h>
+void main() 
+{
+    int n,i;
+    printf("Enter the value if n:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    
+    printf("\n%d * %d=%d",n,i,n*i);
+
+}
