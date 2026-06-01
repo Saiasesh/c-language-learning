65ed47a190e78f986597d8d0fa13b954390fbd9e
Saiasesh <medhosaiasesh@gmail.com>
2026-06-01 09:38:03 +0530

    Update deque.c

--- sem2/deque.c
+++ sem2/deque.c
@@ -1,6 +1,6 @@
-#include<sdtio.h>
+#include<stdio.h>
 #define MAX 5   
-int deque[MAX]
+int deque[MAX];
 int front = -1;
 int rear = -1;
 int isfull()
@@ -11,10 +11,10 @@ int isempty()
 {
     return(front == -1);
 }
-int insertfront(int key)
+void insertfront(int key)
 {
     if(isfull()){
-        printf("Queue is overfull");
+        printf("Queue is overflow\n");
         return;
     }
     if(front==-1){
@@ -26,6 +26,88 @@ int insertfront(int key)
     else{
         front=front-1;
     }
-    deqeu[front]=key;
+    deque[front]=key;
     printf("%d is inserted at front\n",key);
+}
+void insertrear(int key)
+{
+    if(isfull()){
+        printf("Queue is overflow\n");
+        return;
+    }
+    if(front==-1){
+        front=rear=0;
+    }
+    else if(rear==MAX-1){
+        rear=0;
+    }
+    else {
+        rear=rear+1;
+    }
+    deque[rear]=key;
+    printf("%d is inserted at rear\n",key);
+}
+void deletefront()
+{
+    if(isempty()){
+        printf("Queue is Underflow\n");
+        return;
+    }
+    printf("%d is deleted from front\n",deque[front]);
+    if(front==rear){
+        front=rear=-1;
+    }
+    else if(front==MAX-1){
+        front=0;
+    }
+    else{
+        front=front+1;
+    }
+}
+void deleterear()
+{
+    if(isempty()){
+        printf("Queue is Underflow\n");
+        return;
+    }
+    printf("%d is deleted from rear\n",deque[rear]);
+    if(front==rear){
+        front=rear=-1;
+    }
+    else if(rear==0){
+        rear=MAX-1;
+    }
+    else{
+        rear=rear-1;
+    }
+}
+void display()
+{
+    int i;
+    if(isempty()){
+        printf("Queue is Underflow\n");
+        return;
+    }
+    printf("Deque elements are:");
+    i=front;
+    while(1){
+        printf("%d\t",deque[i]);
+        if(i==rear)
+        break;
+        i=(i+1)%MAX;
+    }
+    printf("\n");
+}
+int main()
+{
+    insertrear(10);
+    insertrear(20);
+    insertrear(30);
+    insertfront(40);
+    insertfront(50);
+    display();
+    deletefront();
+    deleterear();
+    display();
+    return 0;
 }
